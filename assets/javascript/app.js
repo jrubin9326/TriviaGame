f06556feba8d9255311a4bd630cf19937035b400
Jennifer Rubin <jenlrubin9326@gmail.com>
2018-10-26 15:13:46 -0400

    trivia questions updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+var questions = [{
+    q1 : {
+        "Which state recieves the most sunshine on average each year?" :
+        ["California", "Florida", "Texas", "Arizona"]
+    }, 
+    q2: {
+        "Hurricanes are named when they sustain speeds of over 74 miles per hour. Severe storms have their names retired. Which of these names has NOT been retired?" :
+        ["Katrina", "Andrew", "Jamie", "Fabian"]
+    },
+    q3: {
+        "Which state is most affected by tornadoes annually?" :
+        ["Kanasas", "Oklahoma", "Lousiana", "Texas"]
+    },
+    q4 : {
+        "On January 23, 1971 the temperatures recorded in Prospect Creek, Alaska were the coldest ever recorded in the USA. What was the recorded temperature (in Fahrenheit)?" :
+        ["-20.5", "-40.3", "-79.8", "-99.6"]
+    },
+    q5: {
+        "In the United States that are on average 708 of what?": 
+        ["Earthquakes", "Lightening Strikes", "Tornados", "Hurricanes"]
+    },
+    q6: {
+        "Cirrus is a type of cloud. What does the literal translation in Latin mean?" :
+        ["Circus", "Snake", "Lock of Hair", "More or Less"]
+    }, 
+    q7 : {
+        "What is the name of the instrument used to predict wind speed?" :
+        ["Anemometer", "Barometer", "Hygrometer","Pyranometer" ]
+    },
+    q8 : {
+        "What is the driest desert on Earth?" :
+        ["The Sahara", "The Kalahari", "The Atacama", "The Negev"]
+    }, 
+    q9 : {
+        "Which city in the United States has the most rainy days?" :
+        ["Columbus, Ohio", "Seattle, Washington", "Miami, Florida", "Cincinnati, Ohio"]
+    },
+    q10  :{
+        "Three to four billion years ago, the Sun emitted what precent of its power as it does today?":
+        ["70%", "30%", "85%", "60%"]
+    }
+}]
+
+// answers Arizona, Jamie, Texas,79.8, torandos, lock of hair, anemomoeter, atacama, seattke, 70%
+
+isGameStarted = false;
